737bec22f514d22c4c827b95ae49b02dbe1c979d
oscar05c4 <oscar.blandon@udea.edu.co>
2025-09-27 21:46:05 -0500

    Versión final del backend para el Sprint 1

--- src/main/java/com/telconova/suportsuite/controller/MessageTemplateController.java
+++ src/main/java/com/telconova/suportsuite/controller/MessageTemplateController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/templates")
 public class MessageTemplateController {
